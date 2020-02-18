1bb7361f4967560f80c0641c8a387c7b4585ee1d
Olivier Schmid <olivier.schmid@mobi.ch>
2020-02-18 15:49:40 +0100

    priv(musicwallet): optimize console.log output

--- src/hooks/useBrowser.ts
+++ src/hooks/useBrowser.ts
@@ -10,18 +10,18 @@ export function useBrowser() {
     }
 
     const openBrowser2 = async (urlToOpen: string) => {
-        console.log('*** will convert url...');
+        console.log('*** useBrowser: will convert url...');
 
         Filesystem.getUri({
             directory: FilesystemDirectory.Data,
             path: 'audio1.wav'
         }).then((getUriResult: GetUriResult) => {
-            console.log(JSON.stringify(getUriResult));
+            console.log('*** useBrowser: getUriResult: ',JSON.stringify(getUriResult));
 
             const path = getUriResult.uri;
             // Browser.open(path, 'application/audio')
-            console.log('*** will open file: ' + path);
-            // Browser.open({url: path})
+            console.log('*** useBrowser: will open file: ' + path);
+            Browser.open({url: path})
         }).then((error) => {
             console.log(error);
         });
